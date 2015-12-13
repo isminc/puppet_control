3422d5f4c680282ec5761a004fa0393871cfb697
Jon Mosco <jonny.mosco@gmail.com>
2015-12-13 12:42:24 -0500

    added nrpe

---
+++ site/profiles/manifests/nrpe.pp
@@ -0,0 +1,19 @@
+# == Class: profiles::nrpe
+#
+class profiles::nrpe {
+
+  class { 'nrpe':
+    allowed_hosts => [ '127.0.0.1', 'nagios.ism.local' ]
+  }
+
+  nrpe::command { 'check_disk':
+    ensure  => present,
+    command => 'check_disk -w 20% -c 10% -p /dev/hda1'
+  }
+
+  nrpe::command { 'check_load':
+    ensure  => present,
+    command => 'check_load -w 15,10,5 -c 30,25,20';
+  }
+
+}
