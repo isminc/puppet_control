03a5c6cad8d0539218417a6f430ce7e4859dcb4e
Jon Mosco <jonny.mosco@gmail.com>
2016-10-29 14:37:40 -0400

    add workaround for virtualenv

--- site/profiles/manifests/puppetboard.pp
+++ site/profiles/manifests/puppetboard.pp
@@ -2,12 +2,18 @@
 #
 class profiles::puppetboard {
 
+  # WORKAROUND
+  class { '::python':
+    virtualenv => 'present',
+    dev        => 'present',
+  }
+
   # Configure Apache on this server
   class { 'apache': }
   # Configure Puppetboard
   class { 'puppetboard':
-    manage_git        => false,
-    manage_virtualenv => false,
+    manage_git        => latest,
+    manage_virtualenv => latest,
   }
   class { 'puppetboard::apache::vhost':
     vhost_name => 'pboard.puppet.ism.local',
